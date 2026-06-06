2d3e04637fe7ecff8d38f1479791b6a0a1474775
EngineMachiner <mfpc98@gmail.com>
2026-06-06 01:27:11 -0500

    sort things in init.lua

--- init.lua
+++ init.lua
@@ -1,45 +1,25 @@
 
-local concat
-
-local function info()
-
-    local path = Astro.Path
-    local info = require(path .. "info")
-
-    local out = "Astro - Version Date: " .. info.VersionDate .. " - " .. info.ID
-    print(out .. '\n\n' .. concat)
-
-end
+Astro = {}          Astro.Path = path or './'
 
 
 local paths = { Vector = true }
 
-local function name(key) return paths[key] and key or key:lower() end
-
 local modules = { "Config", "Type", "Math", "String", "Table", "Vector", "Layout" }
 
-local function subRequire()
+local function name(key) return paths[key] and key or key:lower() end
 
-    local path = Astro.Path
+local path = Astro.Path
 
-    for _, v in ipairs(modules) do
+for _, v in ipairs(modules) do
 
-        local path = path .. name(v)
+    local path = path .. name(v)
 
-        Astro[v] = require(path)
-
-    end
-
-    Astro.info = info
+    Astro[v] = require(path)
 
 end
 
--- This is Astro's loader.
----@param path string?
-return function(path)
 
-    Astro = {}          Astro.Path = path or './'           subRequire()
+---@return AstroInfo
+local function info() end
 
-    local table = Astro.Table           concat = table.concat(Astro)
-
-end
+Astro.info = info
